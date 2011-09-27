e1b7c36b940b34b47b613f6a7b1edb0d587edfd5
lomereiter <lomereiter@gmail.com>
2011-09-28 00:03:23 +0400

    added forward.h

---
+++ forward.h
@@ -0,0 +1,11 @@
+#ifndef FORWARD_H
+#define FORWARD_H
+
+template <typename T> class PrattParser;
+template <typename T> class Symbol;
+template <typename T> struct SymbolDict;
+template <typename T> class Token;
+
+extern const char END_SYMBOL_NAME[];
+
+#endif
